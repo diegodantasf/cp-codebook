3cbc9c8ea09de78f556e69f99490dbd9ef58c1af
Diego Dantas <diego.figueiredo@itec.ufpa.br>
2021-06-11 16:55:55 -0300

    header file

---
+++ misc/header.hpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+#define eq(a, b) (abs(a - b) < EPS)
+#define lt(a, b) ((a + EPS) < b)
+#define gt(a, b) (a > (b + EPS))
+#define le(a, b) (a < (b + EPS))
+#define ge(a, b) ((a + EPS) > b)
+#define $ ios::sync_with_stdio(0);
+#define all(x) x.begin(), x.end()
+ 
+using namespace std;
+using ld = long double;
+using ll = long long int;
+using vi = vector<int>;
+
+// Have fun
+// Read the problem
+// Explain the problem with your words to make sure you got it
+// Write down information about the problem
+// Create a brute force solution
